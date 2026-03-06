ccf8223b388114f10628693b12a1f2bfe60318c0
Jhonlora5 <j.lopez5@sapalomera.cat>
2026-03-06 19:33:24 +0100

    A veure ara...3

--- client/dist/xarxa/connexio.js
+++ client/dist/xarxa/connexio.js
@@ -1,4 +1,5 @@
-export const connexio = new WebSocket(`ws://${window.location.host}`);
+const protocol = window.location.protocol === "https:" ? "wss" : "ws";
+export const connexio = new WebSocket(`${protocol}://${window.location.host}`);
 export function enviarMissatge(dades) {
     connexio.send(JSON.stringify(dades));
 }
